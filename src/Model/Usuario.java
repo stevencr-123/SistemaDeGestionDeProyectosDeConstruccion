0425c4d191d9b4ee0e71033d081eac2dc769020e
stevencr-123 <caraballostevenson5@gmail.com>
2025-03-19 18:29:38 -0500

    Sistema de gestion de proyectos de construccion

---
+++ src/Model/Usuario.java
@@ -0,0 +1,17 @@
+
+package Model;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    protected String dni;
+    protected String tipoId;
+    protected String primerNombre;
+    protected String segundoNombre;
+    protected String primerApellido;
+    protected String segundoApellido;
+    protected String email;
+    protected String direccion;
+    protected LocalDate fechaContrato;
+    //protected Empresa empresa;
+}
