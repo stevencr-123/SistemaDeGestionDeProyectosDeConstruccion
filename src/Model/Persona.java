d587d861aeeb217edeb3e90aed77ebeeacf5ba0e
stevencr-123 <caraballostevenson5@gmail.com>
2025-04-02 14:37:41 -0500

    fix: Ajusté el diagrama de clases añadiendo nuevas clases y enums para facilitar el trabajo. Además creé una super clase Persona que tendrá clases hijas basadas en los tipos de Usuario del sistema.

---
+++ src/Model/Persona.java
@@ -0,0 +1,13 @@
+
+package Model;
+
+public class Persona {
+    //private TipoIdentificacion tipoIdentificacion;
+    private String numeroIdentificacion;
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String email;
+    private String telefono;
+}
