9e1bab2ba13653a5a6b0a3889c023c17245cbd90
stevencr-123 <caraballostevenson5@gmail.com>
2025-04-03 16:19:11 -0500

    feat: Agregué logica para inicio sesion segun el rol de administrador

--- src/Dao/UsuarioDAO.java
+++ src/Dao/UsuarioDAO.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Dao;
 
-/**
- *
- * @author HP
- */
+import Model.RolSistema;
+import Model.Usuario;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.FileReader;
 public class UsuarioDAO {
+
+    private static final String JSON_FILE = "C:\\Users\\HP\\OneDrive\\Documentos\\NetBeansProjects\\GestionParaProyectosDeConstruccion\\src\\Resource\\usuarios.json";
+    
+    public Usuario validarCredenciales (String email, String password){
+        try (FileReader reader = new FileReader(JSON_FILE)){
+            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+            JsonArray usuariosJson = jsonObject.getAsJsonArray("usuarios");
+            
+            for (int i = 0; i < usuariosJson.size(); i++) { 
+                JsonObject usuarioJson = usuariosJson.get(i).getAsJsonObject();
+                
+                String userEmail = usuarioJson.get("email").getAsString();
+                String userPassword = usuarioJson.get("password").getAsString();
+                
+                if (userEmail.equals(email) && userPassword.equals(password)){
+                    RolSistema rol = RolSistema.valueOf(usuarioJson.get("rol").getAsString());
+                    return new Usuario(userEmail, userPassword, rol);
+                    
+                }
+                
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+        
+    }
+    
+    public boolean esAdministrador (Usuario usuario){
+        return usuario != null && usuario.getRol() == RolSistema.ADMINISTRADOR;
+    }
     
 }
+
